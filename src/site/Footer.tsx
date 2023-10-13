351d6c44bb6bbb35b8dd0124c40bc88b98656184
Anna Narushevich <annyit23@gmail.com>
2023-10-13 18:32:45 +0300

    first commit

---
+++ src/site/Footer.tsx
@@ -0,0 +1,11 @@
+
+type FooterPropsType = {
+    titleFooter: string
+}
+
+export const Footer = (props:FooterPropsType) => {
+    return (
+        <div>{props.titleFooter}</div>
+    )
+
+}
---
+++ src/site/Header.tsx
@@ -0,0 +1,11 @@
+
+type HeaderPropsType = {
+    titleHeader: string
+}
+
+export const Header = (props:HeaderPropsType) => {
+    return (
+        <div>{props.titleHeader}</div>
+    )
+}
+
